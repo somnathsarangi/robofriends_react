b5618202858a3ea89c46a1f7dfaaf010650490bf
somnathsarangi <somnathsarangi123@gmail.com>
2021-05-31 13:58:03 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/containers/App.js
@@ -0,0 +1,41 @@
+import React,{ Component } from 'react';
+import Cardlist from '../components/Cardlist';
+import '../containers/App.css';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+// import {robots} from'./robots'; //Because we are exporting only and not default in robots.js
+                                // we need to write in between {}
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {this.setState({robots: users})});
+  }
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+  }
+  render(){
+    const { robots,searchfield } = this.state;
+    const filteredRobots = robots.filter(robots =>{
+      return robots.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    return (
+      <div class='tc'>
+        <h1 className='f1'>Puppy Owners</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
+        <Cardlist robots = {filteredRobots}/>
+        </Scroll>
+      </div>
+    );
+  }
+}
+export default App;
